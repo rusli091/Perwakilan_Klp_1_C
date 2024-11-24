09da7d7812ca70da944870d9bb12565c24a85b5b
Muh Rusli <113271984+rusli091@users.noreply.github.com>
2024-11-24 20:24:28 +0800

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,19 +1,13 @@
 import pandas as pd
 
-# Memuat dataset
 data = pd.read_csv('file_dataset.csv')
 
-# Menampilkan 5 baris pertama dari dataset
 print(data.head())
 
-# Memeriksa informasi dataset
 print(data.info())
 
-# Memeriksa nilai yang hilang
 print(data.isnull().sum())
 
-# Menghapus baris dengan nilai yang hilang (jika ada)
 data_cleaned = data.dropna()
 
-# Menyimpan dataset yang sudah dibersihkan
 data_cleaned.to_csv('dataset_cleaned.csv', index=False)
